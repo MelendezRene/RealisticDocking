297881a516e1a316e3d71d5ae8ff89a2cd2f7d87
MelendezRene <84212336+MelendezRene@users.noreply.github.com>
2026-09-20 19:16:45 -0600

    Bump assembly version to 0.2.0

--- Source/RealisticDocking/AssemblyInfo.cs
+++ Source/RealisticDocking/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyDescription("Realistic docking capture controller for Kerbal Space Program")]
 [assembly: AssemblyCompany("RealisticDocking")]
 [assembly: AssemblyProduct("RealisticDocking")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
